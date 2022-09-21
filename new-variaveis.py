023ea99013e7382b513dc546be6ea801bad388bd
Torres-Neto <torresneto.tn@gmail.com>
2022-09-21 01:18:02 -0300

    exercícios de Python

---
+++ new-variaveis.py
@@ -0,0 +1,16 @@
+# variáveis -> toda variável é um objeto
+
+'''
+nome = 'Torres'
+idade = 34
+peso = 54.3
+
+print(nome, idade, peso)
+print(nome)
+'''
+
+nome = input('Qual é seu nome? ')
+idade = input('Quantos anos você tem? ')
+peso = input('Quanto você pesa? ')
+
+print(nome, idade, peso)
---
+++ scripts-python.py
@@ -0,0 +1,20 @@
+'''
+nome = input('Qual é o seu nome? ')
+print('Olá ', nome,'! Prazer em te conhecer!')
+'''
+
+'''
+print('Digite a sua data de nascimento: ')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu no dia ', dia, ' de ', mes, ' de ', ano,'. Correto?')
+'''
+
+
+print('Digite dois números para soma-los: ')
+numero1 = int(input('Primeiro número = '))
+numero2 = int(input('Segundo número = '))
+soma = numero1 + numero2
+print('A soma é', soma)
+
